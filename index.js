747c86c25e6882009a369c00f429e8a684e8e933
John Joseph <joseph.john33@yahoo.com>
2024-09-09 00:38:08 +0100

    lessonthree submission

---
+++ index.js
@@ -0,0 +1,110 @@
+//Task One
+/* let arr = [1, 3, 5, 10, 12, 13, 16];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+} */
+
+//Task Two
+/* let arr = ["boy", "cat", "dog", "tap", "lap"];
+
+for (let i = arr.length; i > 0; i--) {
+  console.log(arr[i]);
+}
+ */
+
+//Task Three
+// let myObj = {
+//     name: "John",
+//     age: "39",
+//     city: "Abuja",
+//     occupation: "painter"
+// }
+
+// for (const key in myObj) {
+//     console.log(`${key}: ${myObj[key]}`);
+// }
+
+//Task Four
+// let num = 1;
+
+// while (num < 15) {
+//   console.log(num);
+//   num++;
+// }
+
+//Task Five
+/* let num = 1
+
+do {
+    console.log(num);
+    num++
+} while (num <= 12); */
+
+//Task Six
+/* let num = 10
+
+for (let i = 1; i <= num; i++) {
+  console.log(i);
+  if (i == 5) {
+    break
+  }
+} */
+
+//Task Seven
+/* let num = 10;
+
+for (let i = 1; i <= num; i++) {
+  if (i % 2 == 0) {
+    continue;
+  }
+  console.log(i);
+} */
+
+//Task Eight
+/* for (let i = 1; i <= 10; i++) {
+  let row = ""
+
+  for (let j = 1; j <= 10; j++) {
+    
+    row += (i * j) + "\t"
+  }
+  console.log(row)
+}
+ */
+
+//Task Nine
+/* let names = ["jay", "johan", "joseph", "john", "janet"];
+
+forEach()
+ */
+
+let num = [1, 2, 3, 4];
+
+/* num.forEach(num => console.log(num)
+); */
+
+/* num.map (num => console.log(num * 2)
+); */
+
+/* let num2 = num.filter(num => num % 2 == 0);
+console.log(num2); */
+
+//Task Ten
+/* let arr = ["jay", "johan", "joseph", "john", "janet"];
+
+for (const element of arr) {
+  console.log(`my name is ${element}`);
+  
+} */
+
+/* let state = {
+  abia: "Umuahia",
+  rivers: "Port-Harcurt",
+  adamawa: "Yola",
+  edo: "Benin",
+};
+
+for (const [key, value] of Object.entries(state)) {
+  console.log(`i'm from ${key} and i stay in ${value}`);
+} */
